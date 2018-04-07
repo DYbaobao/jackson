6b1c80c5a791eb09e925c3fb8ade40b2279db476
DYbaobao <3295224486@qq.com>
2018-04-07 19:58:35 +0800

    第一次提交

---
+++ src/test/java/com/jackson/json/databinding/ProvinceTest.java
@@ -0,0 +1,38 @@
+package com.jackson.json.databinding;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple Province.
+ */
+public class ProvinceTest
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public ProvinceTest(String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( ProvinceTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
